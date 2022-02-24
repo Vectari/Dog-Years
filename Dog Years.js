b2c53160f9639a65f4967c60b22890570d2a2333
Vectari <93955642+Vectari@users.noreply.github.com>
2022-02-24 23:09:00 +0100

    Add files via upload

---
+++ Dog Years.js
@@ -0,0 +1,11 @@
+const myAge = 26;
+//my age
+let earlyYears = 2;
+earlyYears *= 10.5; 
+let laterYears = myAge - 2;
+// myAge - 2
+laterYears *= 4
+let myAgeInDogYears = earlyYears + laterYears;
+// Show what is my age in dog years
+const myName = "Mateusz".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
